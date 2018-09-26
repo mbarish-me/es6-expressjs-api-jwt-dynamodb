5bed047f3cc3166e8277d936e6abfc25fe5eb690
alowsarwar <ambarish.kumar.l@gmail.com>
2018-09-26 18:00:51 +0530

    adding jwt

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
 import express from 'express';
 import db from './db/db';
 import bodyParser from 'body-parser';
+import jwt from 'jsonwebtoken'; // used to create, sign, and verify tokens
+import bcrypt from 'bcryptjs';
 
 // Set up the express app
 const app = express();
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
+/**
+ * Configure JWT
+ */
+
+let config = require('../config'); // get config file
+
+router.post('/login', function(req, res) {
+
+    let creds = req.body.email && req.body.password;
+    if (!creds)  return res.status(403).send('Email Id & Password required');
+
+    User.findOne({ email: req.body.email }, function (err, user) {
+        if (err) return res.status(500).send('Error on the server.');
+        if (!user) return res.status(404).send('No user found.');
+
+        // check if the password is valid
+        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+        if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+
+        // if user is found and password is valid
+        // create a token
+        var token = jwt.sign({ id: user._id }, config.secret, {
+            expiresIn: 86400 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.status(200).send({ auth: true, token: token });
+    });
+
+});
+
+router.get('/logout', function(req, res) {
+    res.status(200).send({ auth: false, token: null });
+});
+
 
 // get all todos
 app.get('/api/v1/todos', (req, res) => {
