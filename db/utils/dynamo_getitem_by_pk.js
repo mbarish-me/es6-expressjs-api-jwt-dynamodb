51d8b41113feda27a60825aeebdd761aac121065
alowsarwar <ambarish.kumar.l@gmail.com>
2018-09-28 09:30:49 +0530

    using dynamodb doc client

--- db/utils/dynamo_getitem_by_pk.js
+++ db/utils/dynamo_getitem_by_pk.js
@@ -2,38 +2,26 @@
 var AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
 var config = require('./../../config/env');
-var db = new AWS.DynamoDB({endpoint: 'http://localhost:8000',
+var db = new AWS.DynamoDB.DocumentClient({endpoint: 'http://localhost:8000',
     apiVersion: '2012-08-10'});
 
-//
-// var params = {
-//     ExpressionAttributeValues: {
-//         ':s': {N: '2'},
-//         ':e' : {N: '09'},
-//         ':topic' : {S: 'PHRASE'}
-//     },
-//     KeyConditionExpression: 'Season = :s and Episode > :e',
-//     ProjectionExpression: 'Episode, Title, Subtitle',
-//     FilterExpression: 'contains (Subtitle, :topic)',
-//     TableName: 'EPISODES_TABLE'
-// };
-
 var params = {
-    ExpressionAttributeValues: {
-        ':s': {S: 'user1@mail.com'}
+    TableName: 'USER',
+    KeyConditionExpression: '#email = :email',
+    ExpressionAttributeNames: {
+        '#email': 'email'
     },
-    KeyConditionExpression: 'email = :s',
-    // ProjectionExpression: 'first_name, last_name',
-    // FilterExpression: 'contains (Subtitle, :topic)',
-    TableName: 'USER'
+    ExpressionAttributeValues: {
+        ":email": 'user2@mail.com'
+    },
 };
+
 
 // console.log(process.env);
 db.query(params, function(err, data) {
 
     if (err) console.log(err, err.stack); // an error occurred
     else {
-        let response = data.Items.map(item => AWS.DynamoDB.Converter.unmarshall(item));
-        console.log(response);
+        console.log(data);
     }
 });
